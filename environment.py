a252b559895b7d6e9092297de728a6349883ee0a
SinaMousavi <95470638+SinaMousavi@users.noreply.github.com>
2022-07-13 23:42:56 +0430

    Add files via upload

---
+++ environment.py
@@ -0,0 +1,140 @@
+import emoji
+import numpy as np
+
+
+def print_map(map):
+    print("Load Map:")
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == 'X':
+                out = emoji.emojize(':white_large_square:', language='alias')
+            elif map[i][j] == 'S':
+                out = emoji.emojize(':car:', language='alias')
+            elif map[i][j] == 'T':
+                out = emoji.emojize(':fuelpump:', language='alias')
+            elif map[i][j] == 'G':
+                out = emoji.emojize(':checkered_flag:', language='alias')
+            else:
+                out = emoji.emojize(':black_large_square:', language='alias')
+
+            print(out + ' ', end="")
+        print()
+
+
+def load_map(path="map.txt"):
+    result = []
+    with open(path) as file:
+        for line in file:
+            line = list(line.rstrip())
+            result.append(list(line))
+    return result
+
+
+class Environment:
+    X_COST = 5  # Costs for touching the wall. Problem with values < 3.
+    T_COST = -1  # Costs / Reward for driving through the gas station.
+    G_COST = -10  # Costs / Reward for arriving at the goal.
+    TRANS_COST = 1  # General transition costs.
+
+    NUM_ACTIONS = 4
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+    def __init__(self, path):
+        self.map = load_map()
+        print_map(self.map)
+
+        self.maxY = len(self.map)
+        self.maxX = len(self.map[0])
+        self.mapShape = [self.maxY, self.maxX]
+        self.num_states = np.prod(self.mapShape)
+        self.P = {}
+        grid = np.arange(self.num_states).reshape(self.mapShape)
+        it = np.nditer(grid, flags=['multi_index'])
+
+        # Create transitions for all states.
+        while not it.finished:
+            current_state = it.iterindex
+            y, x = it.multi_index
+
+            self.P[current_state] = {action: [] for action in range(self.NUM_ACTIONS)}
+
+            if self.is_terminal(current_state):
+                # No way out of a terminal state. 100% chance of staying in it, without further costs.
+                self.P[current_state][self.UP] = [(1.0, current_state, 0, True)]
+                self.P[current_state][self.RIGHT] = [(1.0, current_state, 0, True)]
+                self.P[current_state][self.DOWN] = [(1.0, current_state, 0, True)]
+                self.P[current_state][self.LEFT] = [(1.0, current_state, 0, True)]
+            else:
+                # Not in a terminal state.
+                up_state = current_state if y == 0 else current_state - self.maxX
+                right_state = current_state if x == (self.maxX - 1) else current_state + 1
+                down_state = current_state if y == (self.maxY - 1) else current_state + self.maxX
+                left_state = current_state if x == 0 else current_state - 1
+
+                self.P[current_state][self.UP] = [(self.probability(current_state, self.UP, up_state),
+                                                   up_state,
+                                                   self.costs(current_state, self.UP, up_state),
+                                                   self.is_terminal(up_state))]
+                self.P[current_state][self.RIGHT] = [(self.probability(current_state, self.RIGHT, right_state),
+                                                      right_state,
+                                                      self.costs(current_state, self.RIGHT, right_state),
+                                                      self.is_terminal(right_state))]
+                self.P[current_state][self.DOWN] = [(self.probability(current_state, self.DOWN, down_state),
+                                                     down_state,
+                                                     self.costs(current_state, self.DOWN, down_state),
+                                                     self.is_terminal(down_state))]
+                self.P[current_state][self.LEFT] = [(self.probability(current_state, self.LEFT, left_state),
+                                                     left_state,
+                                                     self.costs(current_state, self.LEFT, left_state),
+                                                     self.is_terminal(left_state))]
+
+            it.iternext()
+
+    # Translating the state iterator into map-coordinates.
+    def stateindex_to_coordinates(self, index):
+        y = int(index / self.maxX)
+        x = index % self.maxX
+        return y, x
+
+    def is_terminal(self, state):
+        y, x = self.stateindex_to_coordinates(state)
+        return self.map[y][x] == 'G'
+
+    def is_wall(self, state):
+        y, x = self.stateindex_to_coordinates(state)
+        return self.map[y][x] == 'X'
+
+    def is_gas_station(self, state):
+        y, x = self.stateindex_to_coordinates(state)
+        return self.map[y][x] == 'T'
+
+    def is_start(self, state):
+        y, x = self.stateindex_to_coordinates(state)
+        return self.map[y][x] == 'S'
+
+    # Are the costs dependent on the action or the outcome?
+    def costs(self, current_state, action, next_state):
+        costs = self.TRANS_COST
+
+        if self.is_terminal(current_state):
+            costs = 0  # Absorbent Terminal state.
+        elif self.is_terminal(next_state):
+            costs = self.G_COST + 1
+        elif self.is_wall(next_state):
+            costs = self.X_COST  # Wall collision.
+        elif self.is_gas_station(next_state) and not self.is_gas_station(current_state):
+            costs = self.T_COST  # Arriving at gas station. (Only going through)
+
+
+        return costs
+
+    def probability(self, current_state, action, next_state):
+        prob = 0.8
+
+        if self.is_wall(current_state):
+            prob = 0.0
+
+        return prob
---
+++ main.py
@@ -0,0 +1,128 @@
+import colorama
+import emoji
+import environment
+import numpy as np
+
+
+def printV(V, env):
+    n = 0
+    for i in range(env.maxY):
+        for j in range(env.maxX):
+            if env.is_wall(n):
+                color = colorama.Fore.RED
+            elif env.is_gas_station(n):
+                color = colorama.Fore.GREEN
+            elif env.is_terminal(n):
+                color = colorama.Fore.CYAN
+            elif env.is_start(n):
+                color = colorama.Fore.BLUE
+            else:
+                color = colorama.Fore.RESET
+
+            if V[n] == -0:
+                value = 0
+            else:
+                value = V[n]
+
+            print(color + "%+.3f" % value, end="")
+            print(colorama.Fore.RESET + " | ", end="")
+            n += 1
+        print(colorama.Fore.RESET)
+
+
+def printPolicy(policy, env, iter=0):
+    n = 0
+    for i in range(env.maxY):
+        for j in range(env.maxX):
+            arg = np.argmax(policy[n])
+            if env.is_wall(n):
+                out = emoji.emojize(':white_large_square:', use_aliases=True)
+            elif env.is_terminal(n):
+                out = emoji.emojize(':white_check_mark:', use_aliases=True)
+            elif arg == env.UP and iter != 0:
+                out = emoji.emojize(':arrow_up_small:', use_aliases=True)
+            elif arg == env.RIGHT and iter != 0:
+                out = emoji.emojize(':fast_forward:', use_aliases=True)
+            elif arg == env.DOWN and iter != 0:
+                out = emoji.emojize(':arrow_down_small:', use_aliases=True)
+            elif arg == env.LEFT and iter != 0:
+                out = emoji.emojize(':rewind:', use_aliases=True)
+            else:
+                out = emoji.emojize(':new:', use_aliases=True)
+
+            print(out, end=" ")
+            n += 1
+        print()
+
+
+
+
+
+def value_iteration(env, epsilon=0.000001, discount_factor=1.0):
+    def calculate_v_values(V, action, state):
+        [(probability, next_state, cost, done)] = env.P[state][action]
+        return probability * (cost + discount_factor * V[next_state])
+
+    # Initialize states.
+    V_new = np.zeros(env.num_states)
+    #V_new[38] = -24  # Terminal state.
+    policy = np.zeros([env.num_states, env.NUM_ACTIONS])
+    iteration = 0
+    delta = [0]*200
+
+
+    while True:
+        V_old = V_new
+        print("\nIteration:", iteration)
+        print("\nGrid policy:")
+        printPolicy(policy, env, iteration)
+        print("\nGrid Value Function:")
+        printV(V_old, env)
+        print("\nDelta (Biggest Value function difference):", delta[iteration])
+        print("\n====================================================================================")
+
+        iteration += 1
+        delta[iteration] = 0
+
+        V_new = np.zeros(env.num_states)
+
+        # Iterate through all states.
+        for state in range(env.num_states):
+            action_values = np.zeros(env.NUM_ACTIONS)
+
+            # Iterate through all actions of state.
+            for action in range(env.NUM_ACTIONS):
+                # Apply Bellman equation to calculate v.
+                action_values[action] = calculate_v_values(V_old, action, state)
+
+            # Pick the best action in this state (minimal costs).
+            best_action_value = min(action_values)
+
+            # Get biggest difference between best action value and our old value function.
+            delta[iteration] = max(delta[iteration], abs(best_action_value - V_old[state]))
+
+            # Apply Bellman optimality principle.
+            V_new[state] = best_action_value
+
+            # Update the policy.
+            best_action = np.argmin(action_values)
+            policy[state] = np.eye(env.NUM_ACTIONS)[best_action]
+
+        # Check for convergence.
+        if delta[iteration] < epsilon:
+            break
+
+    print("\nFinished! (" + str(iteration) + " Iterations)")
+    print("\nGrid policy:")
+    printPolicy(policy, env, iteration)
+    print("\nGrid Value Function:")
+    printV(V_old, env)
+    print("\nDelta (Biggest Value function difference):", delta[iteration])
+
+    return policy, V_old, delta
+
+
+if __name__ == "__main__":
+    env = environment.Environment("./map")
+    policy, v, deltas = value_iteration(env)
+   
